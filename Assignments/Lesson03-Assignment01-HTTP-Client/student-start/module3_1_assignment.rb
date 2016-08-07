49398bbe7ea0d406b5f6e717f2cf7c94d93c4ac5
Leandro Thimóteo <leandro.s.thimoteo@gmail.com>
2016-08-07 18:15:07 -0300

    Solution of http client assignement

--- Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
+++ Assignments/Lesson03-Assignment01-HTTP-Client/student-start/module3_1_assignment.rb
@@ -1,5 +1,13 @@
-require 'httparty'
+require "httparty"
+require "pry"
 
 class Recipe
+  include HTTParty
+  base_uri "food2fork.com/api"
+  default_params[:key] = ENV["FOOD2FORK_KEY"]
+  format :json
 
+  def self.for(term)
+    get("/search", query: { q: term })["recipes"]
+  end
 end
